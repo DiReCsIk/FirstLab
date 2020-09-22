62332fb0bd0bd12268e574cdd1929815da0322ce
DiReCsIk <kudlik0408@gmail.com>
2020-09-22 13:29:02 +0300

    Commit with empty tests

---
+++ FirstLab/FirstLab/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Remoting.Messaging;
+
+namespace FirstLab
+{
+    
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Vector firstVector = new Vector(3), secondVector = new Vector(3);
+            firstVector.AssignValueByIndex(1, 5);
+            firstVector.AssignValueByIndex(3, 4);
+
+            secondVector.AssignValueByIndex(1, 5);
+            secondVector.AssignValueByIndex(3, 4);
+
+            Console.WriteLine(firstVector);
+            
+        }
+    }
+}
+
+
+
---
+++ FirstLab/TestFirstLab/VectorTest.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using FirstLab;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestFirstLab
+{
+    [TestClass]
+    public class VectorTest
+    {
+        /*
+         * Тест переопределенного метода Equals()
+         */
+        [TestMethod]
+        public void Equals_Test()
+        {
+            throw new Exception();
+        }
+        /*
+        * Тест конструктора копирования
+        */
+        [TestMethod]
+        public void Copy_Constr_Test()
+        {
+            throw new Exception();
+        }
+        /*
+         * Тест на добавление 0 (является удалением)
+        */
+        [TestMethod]
+        public void Check_Add_Elem_With_Zero_In_Data()
+        {
+            throw new Exception();
+        }
+        /*
+         * Тест на добавление элемента под индексом, который уже есть в списке (является заменой элемента)
+         */
+        [TestMethod]
+        public void Check_Add_Elem_With_Same_Index()
+        {
+            throw new Exception();
+        }
+        /*
+        * Тест на добавление элемента
+        */
+        [TestMethod]
+        public void Check_Add_Elem()
+        {
+            throw new Exception();
+        }
+        /*
+        * Проверка переопределенного метода ToString()
+        */
+        [TestMethod]
+        public void Check_To_String()
+        {
+            throw new Exception();
+        }
+        /*
+         * Тест сложения векторов
+         */
+        [TestMethod]
+        public void Check_Vectors_Addition()
+        {
+            throw new Exception();
+        }
+        /*
+         * Тест вычитания векторов
+         */
+        [TestMethod]
+        public void Check_Vectors_Subtraction()
+        {
+            throw new Exception();
+        }
+        /*
+         * Тест умножения вектора на число
+         */
+        [TestMethod]
+        public void Check_Vector_Multiply_With_Number()
+        {
+            throw new Exception();
+        }
+        /*
+        * Проверка модуля пустого вектора
+        */
+        [TestMethod]
+        public void Check_Vector_Absolute_With_Number()
+        {
+            throw new Exception();
+        }
+        /*
+        * Проверка модуля вектора
+        */
+        [TestMethod]
+        public void Check_Vector_Absolute()
+        {
+            throw new Exception();
+        }
+        /*
+        * Проверка скалярного произвидения
+        */
+        [TestMethod]
+        public void Check_Vectors_Scalar_Product()
+        {
+            throw new Exception();
+        }
+        /*
+       * Проверка на сложение пустых векторов
+       */
+        [TestMethod]
+        public void Check_Empty_Vectors_Addition()
+        {
+            throw new Exception();
+        }
+        /*
+        * Проверка на вычитание пустых векторов
+        */
+        [TestMethod]
+        public void Check_Empty_Vectors_Subtraction()
+        {
+            throw new Exception();
+        }
+        /*
+         * Проверка скалярного произвидения пустых векторов
+         */
+        [TestMethod]
+        public void Check_Empty_Vectors_Scalar_Product()
+        {
+            throw new Exception();
+        }
+    }
+
+}
